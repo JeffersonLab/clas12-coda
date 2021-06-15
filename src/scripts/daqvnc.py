3e970d363a266d44f16ca089082ac08135673b6f
baltzell <baltzell@gmx.com>
2021-06-15 08:52:27 -0400

    allow short names and listing in daqvnc.py

--- src/scripts/daqvnc.py
+++ src/scripts/daqvnc.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-import os,sys,stat,json,socket,tempfile,argparse,subprocess,getpass
-
-DEFAULTS=os.getenv('CLON_PARMS')+'/vnc/daqvnc.json'
+import os,sys,glob,stat,json,socket,tempfile,argparse,subprocess,getpass
 
 class DaqVnc():
 
@@ -40,25 +38,53 @@ class DaqVnc():
 
 if __name__=='__main__':
 
+  configurations = None
+
+  if os.getenv('CLON_PARMS') is None:
+    print('Warning:  CLON_PARMS undefined, so full path must be specified for -config.')
+  elif not os.path.isdir(os.getenv('CLON_PARMS')+'/vnc'):
+    print('Warning:  CLON_PARMS/vnc does not exist, so full path must be specified for -config.')
+  else:
+    configurations = [os.path.basename(x)[0:-5] for x in glob.glob(os.getenv('CLON_PARMS')+'/vnc/*.json')]
+
   cli=argparse.ArgumentParser(description='Interface to Hall B DAQ VNC Session.')
-  cli.add_argument('-config',help='path to JSON config (default=%(default)s)',metavar='PATH',default=DEFAULTS)
-  cli.add_argument('command',choices=['stop','start','connect'])
+  cli.add_argument('-config',help='short name or path to configuration file',metavar='PATH',default='daqvnc')
+  cli.add_argument('command',choices=['stop','start','connect','list'])
   args=cli.parse_args(sys.argv[1:])
 
-  dv=DaqVnc(args.config)
-  if args.command=='connect':
-    dv.startClient()
+  if args.command == 'list':
+    print('Available configurations:')
+    print('\n'.join(configurations))
+
   else:
-    if getpass.getuser() != dv.cfg['user']:
-      sys.exit('\nERROR:  This requires user='+dv.cfg['user']+\
-          ', but you are user='+getpass.getuser()+'\n')
-    if dv.cfg['host'] != socket.gethostname():
-      cmd=['ssh',dv.cfg['host'],'python',os.path.realpath(__file__)]
-      cmd.extend(sys.argv[1:])
-      subprocess.call(cmd)
-      sys.exit()
-    if args.command=='stop':
-      dv.killServer()
-    elif args.command=='start':
-      dv.startServer()
+
+    config = args.config
+    if not os.path.isfile(config):
+      if config in configurations:
+        config = '%s/vnc/%s.json'%(os.getenv('CLON_PARMS'),config)
+      else:
+        cli.error('Configuration not found:  '+args.config)
+
+    dv=DaqVnc(config)
+
+    if args.command=='connect':
+      dv.startClient()
+
+    else:
+
+      if getpass.getuser() != dv.cfg['user']:
+        sys.exit('\nERROR:  This requires user='+dv.cfg['user']+\
+            ', but you are user='+getpass.getuser()+'\n')
+
+      if dv.cfg['host'] != socket.gethostname():
+        cmd=['ssh',dv.cfg['host'],'python',os.path.realpath(__file__)]
+        cmd.extend(sys.argv[1:])
+        subprocess.call(cmd)
+        sys.exit()
+
+      if args.command=='stop':
+        dv.killServer()
+
+      elif args.command=='start':
+        dv.startServer()
 
