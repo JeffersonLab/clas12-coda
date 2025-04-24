d52f73c540b39638f02046ffe5ac97b33834d5a6
Sergey Boyarinov <boiarino17@gmail.com>
2025-04-24 16:48:23 -0400

    add dac.s/et_user.c

---
+++ src/dac/dac.s/et_user.c
@@ -0,0 +1,13 @@
+/* et_user.c - extract appropriate data from data banks and fills ET user[] array */
+
+int
+et_user(unsigned int *data, unsigned int *user)
+{
+  int i, len;
+  /*
+  len = data[0];
+  for(i=1; i<=len; i++) printf("0x%08x ",data[i]);
+  printf("\n");
+  */
+  return(0);
+}
